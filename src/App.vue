0a4ce25c79ce0cc27316d2d5d5764cd0868be293
connor <c.stroomberg@gmail.com>
2018-12-27 11:50:46 +0100

    Add menu processing code
    - Add example menu
    - Fix lint errors ( add key to for loops)
    - Add typing to describe menu object as expected from backend

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
   <div id="app">
-    <NavBar></NavBar>
+    <NavBar :molgenis-menu="molgenisMenu"></NavBar>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue'
+import menu from './exampleMenu'
 
 export default Vue.extend({
   name: 'app',
-  components: {
-    HelloWorld,
-    NavBar
+  components: { NavBar },
+  data () {
+    return {
+      molgenisMenu: menu
+    }
   }
 })
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,45 +1,145 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+
+    <a v-if="molgenisMenu.navBarLogo" class="navbar-brand"
+       :href="'/menu/main/'+ molgenisMenu.menu.items[0].href">
+      <img :src="molgenisMenu.navBarLogo" height="20">
+    </a>
+    <a v-else class="navbar-brand" href="#"></a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbar-content">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+
+        <template v-for="item in molgenisMenu.menu.items">
+          <li v-if="item.type === 'PLUGIN' && item.id !== 'home'" :key="item.id"
+              :class="['nav-item', {'active': isSelectedPlugin(item.id)}]">
+
+            <a class="nav-link" :href="'/menu/main/'+item.href">
+              {{ item.label }}
+            </a>
+          </li>
+
+          <li v-else-if="item.id !== 'home'" class="nav-item dropdown" :key="item.id">
+            <a class="nav-link dropdown-toggle" :id="item.id" data-toggle="dropdown" aria-haspopup="true"
+               aria-expanded="false">
+              {{ item.label }}
+            </a>
+            <drop-down-items :parent="item" :items="item.items"/>
+          </li>
+        </template>
+      </ul>
+
+      <ul class="navbar-nav justify-content">
+        <li v-if="molgenisMenu.authenticated && languages.length > 1" class="nav-item">
+          <form id="language-form" class="navbar-form">
+            <select class="nav-link" v-model="selectedLanguage" @change="handleLanguageSelect">
+              <option
+                v-for="language in languages"
+                :key="language.id"
+                :value="language.id"
+                :selected="language.id === selectedLanguage"
+              >
+                {{ language.label }}
+              </option>
+            </select>
+          </form>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link" :href="molgenisMenu.helpLink.href" target="_blank">
+            {{ molgenisMenu.helpLink.label }}
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <form id="logout-form" class="form-inline" method="post" action="/logout">
+            <button v-if="molgenisMenu.authenticated" id="signout-button" type="button" class="btn btn-outline-secondary"
+                    @click="logout">
+              Sign out
+            </button>
+
+            <a v-else class='btn btn-outline-primary' href="/login">Sign in</a>
+          </form>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
+import { MolgenisMenu } from '../types'
+import DropDownItems from './DropDownItems'
+import api from '@molgenis/molgenis-api-client'
 
 export default Vue.extend({
-  name: 'NavBar'
+  name: 'NavBar',
+  props: {
+    molgenisMenu: MolgenisMenu
+  },
+  components: {
+    DropDownItems
+  },
+  data () {
+    return {
+      selectedLanguage: null,
+      languages: [],
+      helperStyle: this.molgenisMenu.topLogo ? {
+        display: 'inline-block',
+        height: '100%',
+        verticalAlign: 'middle'
+      } : undefined
+    }
+  },
+  computed: {
+    topLogoStyle () {
+      return this.molgenisMenu.topLogo ? {
+        maxHeight: this.molgenisMenu.topLogoMaxHeight + 'px',
+        verticalAlign: 'middle'
+      } : undefined
+    },
+    topLogoVueBannerStyle () {
+      return this.molgenisMenu.topLogo ? {
+        height: this.molgenisMenu.topLogoMaxHeight + 'px'
+      } : undefined
+    }
+  },
+  methods: {
+    isSelectedPlugin (plugin) {
+      return plugin === this.selectedPlugin
+    },
+    logout () {
+      if (this.logoutFunction) {
+        this.logoutFunction()
+      }
+      document.getElementById('logout-form').submit()
+    },
+    handleLanguageSelect () {
+      api.post('/plugin/useraccount/language/update?languageCode=' + this.selectedLanguage).then(() => {
+        location.reload(true)
+      })
+    }
+  },
+  mounted () {
+    if (this.molgenisMenu.authenticated) {
+      api.get('/api/v2/sys_Language?q=active==true').then(response => {
+        this.languages = response.items.map(item => {
+          const language = { id: item.code, label: item.name }
+          if (item.code === response.meta.languageCode) {
+            this.selectedLanguage = language.id
+          }
+          return language
+        })
+      }, error => {
+        console.error(error)
+      })
+    }
+  }
 })
 </script>
