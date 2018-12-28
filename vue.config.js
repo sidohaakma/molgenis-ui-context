4aa4a63c37503e6aa201fd557e93c83b636335ff
connor <c.stroomberg@gmail.com>
2018-12-28 10:01:19 +0100

    Add basic end to end test
    Add mock api response to vue.config, via devSever, via express before hook
    Toggle proxy on env mode

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,31 @@ module.exports = {
     })
   },
   devServer: {
-    proxy: 'http://localhost:8080'
+    // Do not proxy in production to allow for mocking api response in e2e test ( e2e tests are run in production mode)
+    proxy: process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080',
+    before: function(app, server) {
+      app.get('/api/v2/sys_Language', function(req, res) {
+        res.json({
+          href: '/api/v2/sys_Language',
+          meta: {
+            languageCode: 'en',
+          },
+          items: [
+            {
+              _href: '/api/v2/sys_Language/de',
+              code: 'de',
+              name: 'Deutsch',
+              active: true
+            },
+            {
+              _href: '/api/v2/sys_Language/en',
+              code: 'en',
+              name: 'English',
+              active: true
+            }
+          ]
+        });
+      });
+    }
   }
 }
