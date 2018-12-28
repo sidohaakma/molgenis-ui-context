bb8110a782bc3b9d567ea34e43c141b731f5daa3
connor <c.stroomberg@gmail.com>
2018-12-28 09:08:07 +0100

    cleanup mocking code

--- tests/unit/__mocks__/@molgenis/molgenis-api-client.js
+++ tests/unit/__mocks__/@molgenis/molgenis-api-client.js
@@ -1,30 +1,23 @@
-const resp = {
-  href: '/api/v2/sys_Language',
-  meta: {
-    languageCode: 'en',
-  },
-  items: [
-    {
-      _href: '/api/v2/sys_Language/de',
-      code: 'de',
-      name: 'Deutsch',
-      active: true
+export default {
+  get: () => Promise.resolve({
+    href: '/api/v2/sys_Language',
+    meta: {
+      languageCode: 'en',
     },
-    {
-      _href: '/api/v2/sys_Language/en',
-      code: 'en',
-      name: 'English',
-      active: true
-    }
-  ]
-}
-
-
-export const mockGet = () => {
-  return new Promise((resolve) => {
-    process.nextTick(() => resolve(resp))
+    items: [
+      {
+        _href: '/api/v2/sys_Language/de',
+        code: 'de',
+        name: 'Deutsch',
+        active: true
+      },
+      {
+        _href: '/api/v2/sys_Language/en',
+        code: 'en',
+        name: 'English',
+        active: true
+      }
+    ]
   })
 }
-
-export default {get: mockGet}
 
