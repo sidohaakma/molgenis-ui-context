ed90d96fd6748bb71f443d681dcd2e71e50a4663
Fleur Kelpin <f.kelpin@umcg.nl>
2019-01-04 23:55:41 +0100

    chore(vue.config.js): Try using the Jenkins agent name as host if present.
    In CI mode, Safari cannot use sauce connect to contact a dev server running on "localhost".
    See https://support.saucelabs.com/hc/en-us/articles/225106887-Safari-and-Internet-Explorer-Won-t-Load-Website-When-Using-Sauce-Connect-on-Localhost
    So as a workaround, contact the dev server using the jenkins agent pod dns instead.

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,8 @@ module.exports = {
     })
   },
   devServer: {
+    // In CI mode, Safari cannot contact "localhost", so as a workaround, run the dev server using the jenkins agent pod dns instead.
+    host: process.env.JENKINS_AGENT_NAME || 'localhost',
     // Do not proxy in production to allow for mocking api response in e2e test ( e2e tests are run in production mode)
     proxy: process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080',
     before: function (app, server) {
