28f86fea08b82b567640a89102efdcebcfca8595
Fleur Kelpin <f.kelpin@umcg.nl>
2019-01-04 16:12:50 +0100

    chore(nightwatch.config.js): Specify custom test_settings for nightwatch.
    Copied from molgenis ui forms.

---
+++ nightwatch.config.js
@@ -0,0 +1,79 @@
+var packageJson = require('package.json')
+
+module.exports = {
+  test_settings: {
+    ci_chrome: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_port: 80,
+      selenium_host: 'ondemand.saucelabs.com',
+      silent: true,
+      username: process.env.SAUCE_CRED_USR,
+      access_key: process.env.SAUCE_CRED_PSW,
+      desiredCapabilities: {
+        name: packageJson.name,
+        build: packageJson.name + '#PR-${CHANGE_ID}-build-${BUILD_NUMBER}',
+        'tunnel-identifier': process.env.TUNNEL_IDENTIFIER,
+        browserName: 'chrome'
+      }
+    },
+    ci_firefox: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_port: 80,
+      selenium_host: 'ondemand.saucelabs.com',
+      silent: true,
+      username: process.env.SAUCE_CRED_USR,
+      access_key: process.env.SAUCE_CRED_PSW,
+      desiredCapabilities: {
+        name: packageJson.name,
+        build: packageJson.name + '#PR-${CHANGE_ID}-build-${BUILD_NUMBER}',
+        'tunnel-identifier': process.env.TUNNEL_IDENTIFIER,
+        browserName: 'firefox'
+      }
+    },
+    ci_ie11: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_port: 80,
+      selenium_host: 'ondemand.saucelabs.com',
+      silent: true,
+      username: process.env.SAUCE_CRED_USR,
+      access_key: process.env.SAUCE_CRED_PSW,
+      desiredCapabilities: {
+        name: packageJson.name,
+        build: packageJson.name + '#PR-${CHANGE_ID}-build-${BUILD_NUMBER}',
+        'tunnel-identifier': process.env.TUNNEL_IDENTIFIER,
+        browserName: 'internet explorer',
+        platform: 'Windows 10',
+        version: '11.103',
+      }
+    },
+    ci_safari: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_port: 80,
+      selenium_host: 'ondemand.saucelabs.com',
+      silent: true,
+      username: process.env.SAUCE_CRED_USR,
+      access_key: process.env.SAUCE_CRED_PSW,
+      desiredCapabilities: {
+        name: packageJson.name,
+        build: packageJson.name + '#PR-${CHANGE_ID}-build-${BUILD_NUMBER}',
+        'tunnel-identifier': process.env.TUNNEL_IDENTIFIER,
+        browserName: 'safari'
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        marionette: true
+      }
+    },
+    safari: {
+      desiredCapabilities: {
+        browserName: 'safari',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      }
+    }
+  }
+}
