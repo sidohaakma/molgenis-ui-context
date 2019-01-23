33522bfa9ee0d77a6d3a97054523db372735afc8
Max <max@elynor.com>
2019-01-23 10:55:14 +0100

    feature/cookiewall

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -14,12 +14,15 @@ module.exports = {
       .assert.cssClassNotPresent('.navbar.navbar-light.bg-light', '.navbar-expand-md')
       .end()
   },
-  'display cookiewall': browser => {
+  'show cookiewall and hide on click': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.navbar')
-      .assert.elementPresent('.navbar-brand')
+      .assert.elementPresent('.jumbotron')
+      .assert.elementPresent('.jumbotron button.btn')
+      .click('.jumbotron button.btn')
+      .pause(600)
+      .assert.elementNotPresent('.jumbotron')
+      .assert.elementNotPresent('.jumbotron button.btn')
       .end()
   }
 }
