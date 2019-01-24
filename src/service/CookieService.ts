e60370d6f9c74a3b89bb63889c81e0045fc92f4a
connor <c.stroomberg@gmail.com>
2019-01-24 13:34:11 +0100

    fix linting issues

--- src/service/CookieService.ts
+++ src/service/CookieService.ts
@@ -4,9 +4,9 @@
  */
 
 export function setCookie (name:string, value:string, days:number) {
-  const expirationDate:Date = new Date
+  const expirationDate:Date = new Date()
   expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000 * days)
-  document.cookie = name + "=" + value + ';path=/;secure: false;expires=' + expirationDate.toUTCString()
+  document.cookie = name + '=' + value + ';path=/;secure: false;expires=' + expirationDate.toUTCString()
 }
 
 export function deleteCookie (name:string) {
@@ -14,7 +14,7 @@ export function deleteCookie (name:string) {
 }
 
 // Extracts a value by key in a "key=value; key=value2;" list
-export function findValueInList(haystack:string, needle:string):string|null {
+export function findValueInList (haystack:string, needle:string):string|null {
   const capturingGroupOffset:number = 2
   let value:RegExpMatchArray|null = haystack.match('(^|;) ?' + needle + '=([^;]*)(;|$)')
   return value ? value[capturingGroupOffset] : null
--- tests/unit/service/CookieService.spec.ts
+++ tests/unit/service/CookieService.spec.ts
@@ -3,28 +3,28 @@ import { setCookie, deleteCookie, findValueInList } from '../../../src/service/C
 describe('CookieService', () => {
   describe('setCookie', () => {
     it('should set a cookie', () => {
-      setCookie('test','true', 1)
+      setCookie('test', 'true', 1)
       expect(document.cookie).toBe('test=true')
     })
-  }),
+  })
   describe('deleteCookie', () => {
     it('should remove a cookie', () => {
-      expect(findValueInList(document.cookie, 'test')).toBe('true');
-      deleteCookie('test');
-      expect(findValueInList(document.cookie, 'test')).toBe(null);
+      expect(findValueInList(document.cookie, 'test')).toBe('true')
+      deleteCookie('test')
+      expect(findValueInList(document.cookie, 'test')).toBe(null)
     })
   })
   describe('findValueInList', () => {
     it('should extract value from semicolon separated list', () => {
-      const listA:string = "key1=value1;key2=value2; key3=value3"
-      const listB:string = "key4:value4;key5=value5;"
+      const listA:string = 'key1=value1;key2=value2; key3=value3'
+      const listB:string = 'key4:value4;key5=value5;'
 
-      expect(findValueInList(listA, "key1")).toBe('value1')
-      expect(findValueInList(listA, "key2")).toBe('value2')
-      expect(findValueInList(listA, "key3")).toBe('value3')
-      expect(findValueInList(listA, "key4")).toBe(null)
-      expect(findValueInList(listB, "key4")).toBe(null)
-      expect(findValueInList(listB, "key5")).toBe('value5')
+      expect(findValueInList(listA, 'key1')).toBe('value1')
+      expect(findValueInList(listA, 'key2')).toBe('value2')
+      expect(findValueInList(listA, 'key3')).toBe('value3')
+      expect(findValueInList(listA, 'key4')).toBe(null)
+      expect(findValueInList(listB, 'key4')).toBe(null)
+      expect(findValueInList(listB, 'key5')).toBe('value5')
     })
   })
 })
