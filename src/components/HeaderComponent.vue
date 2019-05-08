59206f29d9c6c3fb3c12c05c60ad20d808b65c96
connor <c.stroomberg@gmail.com>
2019-05-08 12:36:43 +0200

    Refacor to get ready for sticky header impl
     - Split of header-img into separate component
     - Combine navbar and header-img in header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <header-image v-if="molgenisMenu.topLogo" :imgSrc="molgenisMenu.topLogo" :imgMaxHeight="molgenisMenu.topLogoMaxHeight"/>
+      <cookie-wall cookieName="permissionforcookies" />
+      <nav-bar :molgenis-menu="molgenisMenu" />
+  </header>
+</template>
+
+<script>
+import { MolgenisMenu } from '../types'
+import HeaderImage from './HeaderImage.vue'
+import NavBar from './NavBar.vue'
+import CookieWall from './CookieWall.vue'
+
+export default {
+  name: 'HeaderComponent',
+  components: { NavBar, CookieWall, HeaderImage },
+  props: {
+    molgenisMenu: MolgenisMenu
+  },
+  data: function () {
+    return {}
+  }
+}
+</script>
---
+++ src/components/HeaderImage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="top-logo-vue-banner" :style="topLogoVueBannerStyle" class="bg-light">
+      <span class="mg-top-logo-helper" />
+      <a href="/">
+        <img id="logo-top" class="logo-top" alt="" :src="imgSrc" :style="imgStyle" />
+      </a>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'HeaderImage',
+  props: {
+    imgSrc: {
+      type: String,
+      required: true
+    },
+    imgMaxHeight: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    imgStyle () {
+      return {
+        maxHeight: this.imgMaxHeight + 'px'
+      }
+    },
+    topLogoVueBannerStyle () {
+      return {
+        height: this.imgMaxHeight + 'px'
+      }
+    }
+  }
+})
+</script>
+
+<style>
+  .logo-top {
+    max-width: 90%;
+    vertical-align: middle;
+    border: 0;
+  }
+
+  .mg-top-logo-helper {
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+  }
+</style>
